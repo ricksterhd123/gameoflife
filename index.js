016b6df7798c3af3d2ed48c4c2a3a1bc89131fad
ricksterhd123 <ricksterhd123@gmail.com>
2019-02-02 21:49:21 +0000

    Moved index.html into /public and began to write skelly code for my game of life.

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
 app.listen(port, () => console.log(`App listening on port ${port}`))
